155e464c02542e6bf37572598a49e39fcc340068
Stephen Tran <stephen.v.tran@gmail.com>
2011-10-09 20:57:24 +0000

    Changed DB user for django to be 'apartment' from 'postgres'. Also included Amenity and Unit for admin page from properties app

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,8 @@
-from properties.models import Property
+from properties.models import Property, Amenity, Unit
 from django.contrib import admin
 
-
+admin.site.register(Amenity)
 admin.site.register(Property)
+admin.site.register(Unit)
+
+
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'h3',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'USER': 'apartment',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
