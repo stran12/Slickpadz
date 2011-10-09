fd13713d568fb0842e5f8b534004495ea7c62dfe
Stephen Tran <stephen.v.tran@gmail.com>
2011-10-09 21:10:01 +0000

    Added a view in properties/views and added MY own template directory into TEMPLATE_DIRS of settings.py

--- properties/views.py
+++ properties/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+#
+
+from properties.models import Property, Amenity, Unit
+from django.shortcuts import render_to_response
+
+def index(request, property_id):
+	properties_list = Property.objects.all()
+	return render_to_response('properties/index.html', {'properties_list': properties_list})
+
+
+
