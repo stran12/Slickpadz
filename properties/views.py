39e9e9dcf942d01f975e6962a01bd97e07d1b9de
Stephen Tran <stephen.v.tran@gmail.com>
2011-10-13 21:18:15 +0000

    Got django to finally load static files correctly

--- properties/views.py
+++ properties/views.py
@@ -4,9 +4,17 @@
 from properties.models import Property, Amenity, Unit
 from django.shortcuts import render_to_response
 
+# This next line is IMPORTANT for loading RequestContext into the
+# render_to_response so that we can use STATIC_URL global in templates
+from django.template import RequestContext 
+
+
 def index(request):
 	properties_list = Property.objects.all()
-	return render_to_response('properties/index.html', {'properties_list': properties_list})
+	return render_to_response('properties/index.html',
+							  {'properties_list': properties_list},
+						      context_instance=RequestContext(request),
+							 )
 
 
 
