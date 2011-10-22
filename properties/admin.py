e8b581c90b9084cac492da812a7bae16d1d42c09
Stephen Tran <stephen.v.tran@gmail.com>
2011-10-22 09:09:49 +0000

    Finished preliminary data models that should be sufficient to start the site. Also registered all new data models to be on the admin site to add at least one entry of mock data to design the template for the site.

--- properties/admin.py
+++ properties/admin.py
@@ -1,8 +1,12 @@
-from properties.models import Property, Amenity, Unit
+from properties.models import *
 from django.contrib import admin
 
 admin.site.register(Amenity)
+admin.site.register(PhoneNumber)
+admin.site.register(Source)
+admin.site.register(Manager)
 admin.site.register(Property)
 admin.site.register(Unit)
+admin.site.register(PropertyType)
 
 
--- properties/models.py
+++ properties/models.py
@@ -4,51 +4,80 @@ from location.models import State, City
 
 import datetime
 
-
-class Property(models.Model):
-	city = models.ForeignKey(City)
-	name = models.CharField(max_length=50)
-	latitude  = models.IntegerField()
-	longitude = models.IntegerField()
-	address	  = models.CharField(max_length=50)
-	price     = models.IntegerField()
-	def __unicode__(self):
-		return self.name
-
-	
-class Unit(models.Model):
-	number 	= models.CharField(max_length=10)
-	bed     = models.IntegerField()
-	bath	= models.IntegerField()
-	def __unicode__(self):
-		return self.number
-
-class Amenity(models.Model):
-	prop = models.ManyToManyField(Property)
-	name = models.CharField(max_length=50)
-	pet_choices = (
-		('c','Cats'),
-		('d','Dogs'),
-		('b','Both'),
-		('n','None'),
-	)
-	#pets = models.CharField(max_length=1, choices = pet_choices)
-	def __unicode__(self):
-		return self.name
-
 class PhoneNumber(models.Model):
 	number = models.CharField(max_length=10)
 	ext    = models.CharField(max_length=6)
 	def __unicode__(self):
 		return self.number + ' ' + self.ext
 
-
-
-class Sources(models.Model):
+class Source(models.Model):
 	name = models.CharField(max_length=50)
 	url  = models.CharField(max_length=50)
 	def __unicode__(self):
 		return self.name
+
+
+class Manager(models.Model):
+	name	= models.CharField(max_length=50)
+	phone	= models.ForeignKey(PhoneNumber)
+	url		= models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.name
+
+class PropertyType(models.Model):
+	name = models.CharField(max_length=15)
+	def __unicode__(self):
+		return self.name
+
+
+class Property(models.Model):
+	name	 	= models.CharField(max_length=50)
+	address	  	= models.CharField(max_length=50)
+	city 		= models.ForeignKey(City)
+	state		= models.ForeignKey(State)
+	zip_code	= models.IntegerField()
+	manager     = models.ForeignKey(Manager)
+	phone		= models.OneToOneField(PhoneNumber)
+	source		= models.ForeignKey(Source)
+	prop_url	= models.CharField(max_length=200)
+	prop_type	= models.ForeignKey(PropertyType)
+	latitude  	= models.FloatField()
+	longitude  	= models.FloatField()
+	def __unicode__(self):
+		return self.name
+	
+class Unit(models.Model):
+	prop    = models.ForeignKey(Property)
+	number 	= models.CharField(max_length=10)
+	bed     = models.IntegerField()
+	bath	= models.IntegerField()
+	sqft	= models.IntegerField()
+	price_low = models.IntegerField()
+	price_high= models.IntegerField()
+	deposit = models.IntegerField()
+	def __unicode__(self):
+		return self.number
+
+class Amenity(models.Model):
+	prop = models.ManyToManyField(Property)
+	name = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
