279aa47c2d09f7d2ea673be3126c549ac01d1303
Ilya Godes <igodes@yahoo.com>
2011-10-28 05:45:20 +0000

    copy search location to another field

--- properties/static/js/spz.home.js
+++ properties/static/js/spz.home.js
@@ -7,7 +7,8 @@ spz.home = {
         //TODO: move this to spz.js, b/c there are search fields on other pages
         $("#main-search > .term, #secondary-search > .term").focus(spz.home.clearSearchField)
                                                             .autocomplete({
-                                                                source: serverContext.suggestList
+                                                                source: serverContext.suggestList,
+                                                                select: spz.home.copySearchVal
                                                             });
     },
     clearSearchField: function(evt) {
@@ -15,6 +16,9 @@ spz.home = {
         if ($field.val() === 'City, State or ZIP') {
             $field.val('');
         }
+    },
+    copySearchVal: function(evt) {
+        $("#main-search > .term, #secondary-search > .term").not($(evt.target)).val($(evt.target).val());
     }
 };
 $(document).bind('spz:initialized', spz.home.init);
