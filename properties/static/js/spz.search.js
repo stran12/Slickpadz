0df63011aa3372bd66427a5107cdc2c7154ea752
Ilya Godes <igodes@yahoo.com>
2011-11-03 03:36:57 +0000

    slider shit

--- properties/static/js/spz.search.js
+++ properties/static/js/spz.search.js
@@ -4,6 +4,7 @@
 spz.search = {
     init: function() {
         spz.search.initMap();
+        spz.search.initSliders();
         spz.search.zebraStripe($('#data-grid'));
     },
     /**
@@ -17,6 +18,21 @@ spz.search = {
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             },
             map = new google.maps.Map($("#map_canvas")[0], myOptions);
+    },
+    initSliders: function() {
+        var $rentSlider = $("#rent-range-slider");
+
+        $rentSlider.slider({
+            range: true,
+            min: 0,
+            max: 5000,
+            step: 10,
+            values: [100, 500],
+            slide: function(evt, ui) {
+                $("#rent-range").html("$"+ui.values[0]+" - $"+ui.values[1]);
+            }
+        });
+        $("#rent-range").html("$"+$rentSlider.slider('values',0)+" - $"+$rentSlider.slider('values',1));
     },
     zebraStripe: function($table) {
         $table.find('tbody > tr:odd').addClass('odd');
